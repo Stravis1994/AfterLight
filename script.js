a0822013557f63ef59a5ecf18258c95270719846
Stravis1994 <sean.travis1994@gmail.com>
2026-03-26 14:14:34 +1100

    Changes to be committed: 	modified:   dream-journal.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ addEventListener("DOMContentLoaded", function () {
 
     signUpForm.addEventListener("submit", function (event) {
         event.preventDefault();
-        const name = document.getElementById("username").value;
+        const name = document.getElementById("name").value;
         const surname = document.getElementById("surname").value;
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
@@ -16,7 +16,8 @@ addEventListener("DOMContentLoaded", function () {
             ("Password:", password);
 
         // For demonstration, we'll just load the home page
-        window.location.href = "index.html";
+        window.location.href = "user-page.html";
+        alert("Sign up successful! Welcome, " + name + " " + surname + "!");
     });
 });
 
@@ -47,8 +48,37 @@ addEventListener("DOMContentLoaded", function () {
         // Here you would typically send the data to your server for processing
         console.log("Dream Details:", dreamDetails);
 
-        // For demonstration, we'll just reset the form
+        // For demonstration, we'll just reset the form and add the dream to the list
+        dreamList.innerHTML += `<li>${dreamDetails}</li>`;
         dreamForm.reset();
         alert("Dream submitted successfully!");
+
     });
 });
+
+addEventListener("DOMContentLoaded", function () {
+    const dreamList = document.getElementById("dreamList");
+    const dreams = [
+        "I was flying over a beautiful landscape.",
+        "I was being chased by a mysterious figure.",
+        "I found myself in a magical forest.",
+    ];
+
+    dreams.forEach(function (dream) {
+        const listItem = document.createElement("li");
+        listItem.textContent = dream;
+        dreamList.appendChild(listItem);
+    });
+});
+
+addEventListener("DOMContentLoaded", function () {
+    const addimage = document.getElementById("addImage");
+
+    addimage.addEventListener("click", function (event) {
+        event.preventDefault();
+        // Here you would typically open a file dialog to select an image
+        console.log("Add Image button clicked!");
+        alert("Add Image functionality is not implemented yet.");
+    });
+});
+
